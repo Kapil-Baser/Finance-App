84bd5da267c69308f01a7c27e8bc0d604a303a04
Kapil-Baser <kapil.kumar.baser@gmail.com>
2024-09-13 08:01:53 +0530

    test file and reading it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: financeapp <File Name>");
             return;
         }
-        System.out.println("Hello, world!");
+
+        // Reading the given file
+        ArrayList<String> list = new ArrayList<>();
+        try (Scanner scan = new Scanner(Paths.get(args[0]))) {
+            while (scan.hasNextLine()) {
+                list.add(scan.nextLine());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println(list);
     }
 }
