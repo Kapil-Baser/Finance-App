603771ae95fc90e651a3364c736994af6a82261f
Kapil-Baser <kapil.kumar.baser@gmail.com>
2024-09-13 07:31:47 +0530

    init commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: financeapp <File Name>");
+            return;
+        }
         System.out.println("Hello, world!");
     }
 }
