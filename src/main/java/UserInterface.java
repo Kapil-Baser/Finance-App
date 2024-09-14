00f5ae5542a66334ff2e20f86015c22f82a6d279
Kapil-Baser <kapil.kumar.baser@gmail.com>
2024-09-15 04:59:42 +0530

    ui class

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserInterface {
+    private ArrayList<Transactions> transactionList;
+    private Scanner scanner;
+
+    public UserInterface(Scanner scan) {
+        this.scanner = scan;
+        this.transactionList = new ArrayList<>();
+    }
+
+    public void start() {
+
+    }
+}
