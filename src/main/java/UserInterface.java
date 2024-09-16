81d44e30fbd1b3fe5c938bdfff03010fc122952c
Kapil-Baser <kapil.kumar.baser@gmail.com>
2024-09-16 07:05:04 +0530

    ui class

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -13,12 +13,34 @@ public class UserInterface {
 
     public void start() {
         String header = scanner.nextLine();
+
         while (scanner.hasNextLine()) {
             String transaction = scanner.nextLine();
             String[] str = transaction.split("[\",]+");
             Transactions statement = new Transactions(str[3], Double.parseDouble(str[4]), str[1]);
             this.transactionList.add(statement);
         }
+        listCommands();
+        processCommands();
     }
 
+    private static void processCommands() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String command = scan.nextLine();
+            if (command.equals("quit")) {
+                break;
+            }
+        }
+    }
+
+    public static void listCommands() {
+        System.out.println("Commands: ");
+        System.out.println("1. list - to list all the transactions");
+        System.out.println("2. quit - to quit");
+    }
+
+    public static void printHeader() {
+        System.out.print("Transaction date\t\t Description\t\t Cost\n");
+    }
 }
