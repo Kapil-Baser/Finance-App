1cfb489dc92383e7ef36319792ddff5a134fc3d5
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-04-24 03:45:39 +0530

    Wrapping the IOException in UncheckedIOException

--- src/main/java/com/finance/app/FinanceApp.java
+++ src/main/java/com/finance/app/FinanceApp.java
@@ -1,13 +1,16 @@
 package com.finance.app;
 
+import com.finance.app.ui.UserInterface;
+
 import java.io.*;
 import java.util.Date;
 
 
 public class FinanceApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println(new Date());
+
         if (args.length != 1) {
             System.out.println("Usage: financeapp <File Name>");
             return;
@@ -16,8 +19,8 @@ public class FinanceApp {
         try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
             UserInterface ui = new UserInterface(reader);
             ui.start();
-        } catch (FileNotFoundException e) {
-            throw new FileNotFoundException(String.format("File: %s not found.", args[0]));
+        } catch (IOException ex) {
+            throw new UncheckedIOException(String.format("File: %s not found.", args[0]), ex);
         }
 
     }
